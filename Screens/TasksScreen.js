1e550f9bc207905e304663608c9ac110cc82a313
Shaun Gregory Stummer <141109489+TAFESHAUN@users.noreply.github.com>
2025-11-06 13:08:20 +1100

    :sparkles: Update feat: TasksScreen.js added in ADD CRUD functionality and UI form

--- Screens/TasksScreen.js
+++ Screens/TasksScreen.js
@@ -1,20 +1,54 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
-import {Text, List, Divider} from 'react-native-paper';
+import {Text, List, Divider, TextInput, Button} from 'react-native-paper';
 
 //*SECTION - Task Screen Page
 export default function TasksScreen() {
-    //MOCK DATA START
+    //NOTE - MOCK DATA START
     const [tasks, setTasks] = React.useState([
         { id: 1, text: 'Finish assessment'},
         { id: 2, text: "Review git commits"},
         { id: 3, text: "Test add new task"},
     ]);
 
+    //ANCHOR - Task Add
+    const [taskText, setTaskText] = React.useState('');
+    const addTask = () => {
+        const trimmed = taskText.trim();
+        if (!trimmed) {
+        Alert.alert('Validation', 'Task cannot be empty.');
+        return;
+        }
+        if (trimmed.length > 30) {
+        Alert.alert('Validation', 'Task must be 30 characters or less.');
+        return;
+        }
+        setTasks((prev) => [...prev, { id: Date.now(), text: trimmed }]);
+        setTaskText('');
+        //Keyboard.dismiss();
+    };
+
     return(
         <View style={styles.container}>
             <Text variant='headlineMedium' styles={styles.marg16}>
                 Tasks List</Text>
+            {/* TASK FORM */} 
+            <View style={styles.formRow}>
+                <TextInput 
+                    label="Add a task"
+                    value={taskText}
+                    onChangeText={setTaskText}
+                    mode="outlined"
+                    left={<TextInput.Icon icon="pencil" />}
+                    style={styles.input}
+                    maxLength={30}
+                />
+                <Button mode="contained" onPress={addTask} accessibilityLabel='Add Task Button'>
+                    Add Task
+                </Button>
+            </View>
+
+            {/* TASK LIST */}    
             <List.Section>
                 {tasks.length === 0 && <Text>No Tasks Added</Text>}
                 {tasks.map((item, index) => (
@@ -36,6 +70,8 @@ export default function TasksScreen() {
 //*SECTION - Styles Section
 const styles = StyleSheet.create({
     container: {flex: 1, paddingTop: 50, justifyContent: 'top', alignItems: 'center'},
+    formRow: {flexDirection: 'row', gap: 12, alignItems: 'center', marginBottom: 16 },
     marg16:{marginBottom: 16},
+    input: {flex: 1, margin: 5}
 })
 //!SECTION
